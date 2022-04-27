d6d665d5816784c783ccf9c67b111ba8ca315be5
Octavian-Roman <work.octavian@gmail.com>
2022-04-27 15:38:22 +0200

    Demo HTML + CSS

---
+++ src/main/java/com/bouncesite/BounceSiteApplication.java
@@ -0,0 +1,13 @@
+package com.bouncesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BounceSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BounceSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bouncesite/BounceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouncesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BounceSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
